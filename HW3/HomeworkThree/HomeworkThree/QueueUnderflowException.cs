80d5311860bc072f9b0b82f0dec8f0eb564e5512
mjklienst <mklienstuber@mail.wou.edu>
2019-10-17 18:10:08 -0700

    Added new cs file, edited Java code

---
+++ HW3/HomeworkThree/HomeworkThree/QueueUnderflowException.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace HomeworkThree
+{
+
+
+/**
+ * A custom unchecked exception to represent situations where 
+ * an illegal operation was performed on an empty queue.
+ */
+public class QueueUnderflowException : SystemException
+    {
+  public QueueUnderflowException()
+{
+    base();
+}
+
+public QueueUnderflowException(String message)
+{
+    base(message);
+}
+}
+
+}
