68822eafbf310d6f8bc226bee1ae9cc44ba1dc07
mjklienst <mklienstuber@mail.wou.edu>
2019-11-02 17:49:59 -0700

    Edited form/input data

--- HW5/Homework5_2/Homework5_2/Models/Homework.cs
+++ HW5/Homework5_2/Homework5_2/Models/Homework.cs
@@ -11,29 +11,31 @@ namespace Homework5_2.Models
     {
         public int ID { get; set; }
 
-        [Required]
+        [Required, Display(Name = "Homework Title: ")]
         [StringLength(64)]
         public string HomeworkTitle { get; set; }
 
-        [Required]
+        [Required, Display(Name = "Notes: ")]
         [StringLength(128)]
         public string Notes { get; set; }
 
+        [Required, Display(Name = "Due Date: ")]
+        [DisplayFormat(ApplyFormatInEditMode = true, DataFormatString = "{0:MM/dd/yyyy}")]
         [Column(TypeName = "date")]
         public DateTime DueDate { get; set; }
 
+        //[DisplayFormat(ApplyFormatInEditMode = true, DataFormatString = "{0:HH:mm}")]
+        [Required, Display(Name = "Due Time: ")]
         public TimeSpan DueTime { get; set; }
 
-        [Required]
+        [Required, Display(Name = "Department: ")]
         [StringLength(3)]
         public string Department { get; set; }
 
+        [Required, Display(Name = "Course Number: ")]
         public int CourseNum { get; set; }
 
- 
-
-        [Required]
-        //public string Priority { get; set; }
+        [Required, Display(Name = "Priority: ")]
         public IEnumerable<System.Web.Mvc.SelectListItem> Priority { get; set; }
     }
 }
