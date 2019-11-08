9b304f090326df96a5930fd199344d842e96971d
mjklienst <mklienstuber@mail.wou.edu>
2019-11-07 17:05:10 -0800

    First commit in feature1 branch, input text box works

---
+++ HW6/Homework6/Homework6/Controllers/HomeController.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using Homework6.DAL;
+using Homework6.Models;
+using Homework6.Models.ViewModels;
+
+
+namespace Homework6.Controllers
+{
+    public class HomeController : Controller
+    {
+
+        WWIContext db = new WWIContext();
+
+        [HttpGet]
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult Index(string searchString)
+        {
+            ViewBag.searchResult = searchString;
+
+            List<StockItemViewModel> stockList = db.StockItems.Select(p => new StockItemViewModel
+            {
+                StockItemName = p.StockItemName,
+            }).Where(sn => sn.StockItemName.Contains(searchString)).ToList();
+
+            if (stockList.Count > 0)
+            {
+                ViewBag.toggle = 1;
+                ViewBag.searchResult = "Stock items matching your search: \"" + searchString + "\"";
+            }
+            else
+            {
+                ViewBag.searchResult = "I'm sorry, your search returned no results";
+                return View(stockList);
+            }
+
+            return View(stockList);
+
+        }
+
+        public ActionResult About()
+        {
+            ViewBag.Message = "Your application description page.";
+
+            return View();
+        }
+
+        public ActionResult Contact()
+        {
+            ViewBag.Message = "Your contact page.";
+
+            return View();
+        }
+    }
+}
---
+++ HW6/Homework6/Homework6/Models/ViewModels/StockItemViewModel.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using Homework6.Models;
+using Homework6.DAL;
+
+namespace Homework6.Models.ViewModels
+{
+    public class StockItemViewModel
+    {
+        public string StockItemName { get; set; }
+        public string Size { get; set; }
+        public decimal? RecommendedRetailPrice { get; set; }
+        public decimal TypicalWeightPerUnit { get; set; }
+        public int LeadTimeDays { get; set; }
+
+        [Column(TypeName = "datetime2")]
+        public DateTime ValidFrom { get; set; }
+        public string CustomFields { get; set; }
+
+        [DatabaseGenerated(DatabaseGeneratedOption.Computed)]
+        public string Tags { get; set; }
+        public byte[] Photo { get; set; }
+
+
+    }
+
+}
